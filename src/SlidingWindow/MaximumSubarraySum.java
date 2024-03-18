74c72bf0a23eaf3f3945f97d15b1c01106069af0
Imad-7000 <imad.99211@gmail.com>
2024-03-18 14:20:56 +1030

    Sliding Window - 2461 - solution

--- src/SlidingWindow/MaximumSubarraySum.java
+++ src/SlidingWindow/MaximumSubarraySum.java
@@ -1,5 +1,66 @@
+/*
+ * 2461. maximum sum of distinct subarrays with length k
+ * https://leetcode.com/problems/maximum-sum-of-distinct-subarrays-with-length-k/description/
+ */
+
+
 package SlidingWindow;
+
+import java.util.HashSet;
 
 public class MaximumSubarraySum {
 
+    public static long maximumSubarraySum(int[] nums, int k){
+        long answer = 0;
+
+        int i = 0; 
+
+        while(i <= nums.length - k){
+            
+             HashSet<Integer> seen = new HashSet<>();
+            long temp = 0;
+            int j = i;
+            while (j < i + k) {
+                if(!seen.contains(nums[j])){
+                    seen.add(nums[j]);
+                    temp += nums[j];
+                }
+                j++;
+            }
+            
+            if(seen.size() == k && temp > answer)
+            answer = temp;
+            i++;
+        }
+        return answer;
+    }
+
+    public static long maximumSubarraySumv2(int[] nums, int k){
+        long answer = 0;
+        int i = 0; 
+        HashSet<Integer> seen = new HashSet<>();
+        System.out.println("YOLO");
+        int temp = 0;
+        int j = 0;
+        while (i <= nums.length - k) {
+            seen.add(nums[i]);
+            while (j < i + k) {
+                if(!seen.contains(nums[j])){
+                    seen.add(nums[j]);
+                }
+                
+                temp += nums[j];
+                j++;
+            }
+            if(seen.size() == k && temp > answer){
+               
+                answer = temp;
+                }
+            seen.remove(nums[i]);
+            temp -= nums[i];
+            i++;
+        }
+
+        return answer;
+    }
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,10 +1,16 @@
 import Strings.*;
+import SlidingWindow.*;
 public class Tester {
 
     public static void main(String[] args) {
        // System.out.println(ValidNumbers.isNumber("-6"));
 
        //System.out.println(FirstOccurrence.firstOcc("mississippi", "issip"));
-        System.out.println(Strings.SlidingWindow.MinimumWindow.minWindow("ADOBECODEBANC", "ABC"));
+        int[] nums1 = new int[10000];
+        for(int i = 0; i < 10000; i++){
+            nums1[i] = i + 1;
+        }
+       int[] nums = new int[]{1,5,4,2,9,9,9};
+       System.out.println(MaximumSubarraySum.maximumSubarraySumv2(nums1, 10000));
     }
 }
