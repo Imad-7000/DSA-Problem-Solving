c2378399498da848218216d02911a409eaca9251
Imad-7000 <imad.99211@gmail.com>
2024-03-25 20:50:12 +1030

    Stock Spanner Problem 901 leetcode

--- src/Tester.java
+++ src/Tester.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import Stacks.*;
 import Stacks.NextGreater.SmallerElement.NextGreaterElement;
+import Stacks.NextGreater.SmallerElement.StockSpanner;
 public class Tester {
 
     public static void main(String[] args) { 
@@ -26,9 +27,20 @@ public class Tester {
 
 
         int[] arr = new int[]{1,3,2,0,0,1,4, 10, 9};
-        arr = NextGreaterElement.nextGreater(arr);
+        //arr = NextGreaterElement.nextGreater(arr);
         for(int num: arr)
             System.err.print(num + " ");
+
+
+        StockSpanner stockSpanner = new StockSpanner();
+        System.out.println(stockSpanner.next(100));
+        System.out.println(stockSpanner.next(80));
+        System.out.println(stockSpanner.next(60));
+        System.out.println(stockSpanner.next(70));
+        System.out.println(stockSpanner.next(60));
+        System.out.println(stockSpanner.next(75));
+        System.out.println(stockSpanner.next(85));
+
 
     }
 }
