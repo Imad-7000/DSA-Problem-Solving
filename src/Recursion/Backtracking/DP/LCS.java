689ae7362a147a5a53d3daae17a3492579058bcd
Imad-7000 <imad.99211@gmail.com>
2024-04-01 20:23:06 +1030

    LCS - count is good, lcs still needs work

---
+++ src/Recursion/Backtracking/DP/LCS.java
@@ -0,0 +1,50 @@
+/*
+ * 
+ * 
+ * LEAST COMMON SUBSTRING
+ * 
+ * given two substrings, return the least common substring 
+ * 
+ * 
+ * Variant 1
+ */
+
+package Recursion.Backtracking.DP;
+
+public class LCS {
+
+    public static int lcs(int m, int n, String s1, String s2, int[][] memo){
+        if(m ==0 || n == 0)
+        {
+          return 0;
+        }
+        if(memo[m][n] != -1)
+            return memo[m][n];
+        int ans = 0;
+
+        if(s1.charAt(m- 1) == s2.charAt(n -1))
+            return 1 +  lcs(m- 1, n - 1, s1, s2, memo);
+        
+        ans = Math.max(lcs(m - 1, n , s1,s2,memo), lcs(m, n - 1, s1, s2, memo));
+        memo[m][n] = ans;
+        return ans;
+    }
+
+    public static String longest(String s1, String s2, int m, int n, int[][] memo){
+        StringBuilder str = new StringBuilder();
+        while (m >= 0 || n >=0 ) {
+            if(s1.charAt(m) == s2.charAt(n)){
+                m--;
+                n--;
+                str.append(s1.charAt(m));
+            }
+            else{
+                if ( n > 0)
+                    n--;
+                else
+                    m--;
+            }
+        }
+        return str.reverse().toString();
+    }
+}
--- src/Tester.java
+++ src/Tester.java
@@ -2,8 +2,14 @@ import Recursion.Backtracking.*;
 public class Tester {
 
     public static void main(String[] args) {
-        int[][] board = new int[3][3];
-        boolean val =  NQueens.nQueens(0, board); 
-        System.out.println(val);
+
+        int[][] memo = new int[7][8];
+        for(int i = 0; i < memo.length; i++)
+            for(int j = 0; j < memo[i].length; j++)
+                memo[i][j] = -1;
+
+        System.out.println(Recursion.Backtracking.DP.LCS.lcs(6, 7, "aggtab", "gtxxayb", memo));
+
+        System.out.println(Recursion.Backtracking.DP.LCS.longest("aggtab","gtxxayb",5,6,memo));
     }
 }
