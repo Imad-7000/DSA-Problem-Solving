bf7794309bb0748414a0fc007f9290402be375dd
Imad-7000 <imad.99211@gmail.com>
2024-04-02 17:51:52 +1030

    Largest merge of two strings 1754

--- src/Tester.java
+++ src/Tester.java
@@ -8,9 +8,10 @@ public class Tester {
         //     for(int j = 0; j < memo[i].length; j++)
         //         memo[i][j] = -1;
 
-        int[] arr = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
+       // int[] arr = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
 
-        System.out.println(Arrays.PrefixSum.TrappingRainWater.trap(arr));
+       // System.out.println(Arrays.PrefixSum.TrappingRainWater.trap(arr));
+       System.out.println(Recursion.LargestMerge.largestMerge("cabaa", "bcaaa"));
 
     }
 }
