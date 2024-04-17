824bdaddc932941da2ce4e0aa3dae7b89efe3ab6
Imad-7000 <imad.99211@gmail.com>
2024-04-17 20:26:08 +0930

    New Template - Theory

---
+++ src/Arrays/MinimumTimeToFinishAllJobs.java
@@ -0,0 +1,67 @@
+/*
+ * 1723. Find Minimum Time to Finish All Jobs
+ * https://leetcode.com/problems/find-minimum-time-to-finish-all-jobs/description/
+ * 
+ * You are given an integer array jobs, where jobs[i] is the amount of time it takes to complete the ith job.
+
+There are k workers that you can assign jobs to. Each job should be assigned to exactly one worker.
+ The working time of a worker is the sum of the time it takes to complete all jobs assigned to them. 
+ Your goal is to devise an optimal assignment such that the maximum working time of any worker is minimized.
+
+Return the minimum possible maximum working time of any assignment.
+
+ 
+
+Example 1:
+
+Input: jobs = [3,2,3], k = 3
+Output: 3
+Explanation: By assigning each person one job, the maximum time is 3.
+Example 2:
+
+Input: jobs = [1,2,4,7,8], k = 2
+Output: 11
+Explanation: Assign the jobs the following way:
+Worker 1: 1, 2, 8 (working time = 1 + 2 + 8 = 11)
+Worker 2: 4, 7 (working time = 4 + 7 = 11)
+The maximum working time is 11.
+ 
+
+Constraints:
+
+1 <= k <= jobs.length <= 12
+1 <= jobs[i] <= 107
+ * 
+ */
+
+package Arrays;
+
+import java.util.Arrays;
+
+public class MinimumTimeToFinishAllJobs {
+    int globalMin = Integer.MAX_VALUE;
+    public int minimumTimeRequired(int[] jobs, int k) {
+        int[] workers = new int[k];
+        helper(jobs, workers, 0);
+        return globalMin;
+    }
+    
+    public void helper(int[] jobs, int[] worker, int current){
+        if(current >= jobs.length){
+            int val = Arrays.stream(worker).max().orElse(0);
+            globalMin = Math.min(val, globalMin);
+            return;
+        }
+
+        if(Arrays.stream(worker).max().getAsInt() >= globalMin)
+            return;
+            
+        for(int i = 0 ; i < worker.length; i++){
+            worker[i] += jobs[current];
+            helper(jobs, worker, current + 1);
+            worker[i] -= jobs[current];
+            if(worker[i] == 0)
+                break;
+        }
+    }
+}
