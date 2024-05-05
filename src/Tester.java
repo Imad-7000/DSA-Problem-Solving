fa99a8353d01b59d4e7f560b15f4d5390394ba87
Imad-7000 <imad.99211@gmail.com>
2024-05-05 14:33:49 +0930

    Minimum number of Vertices to reach entire graph

--- src/Tester.java
+++ src/Tester.java
@@ -1,18 +1,47 @@
+import java.util.*;
+
+import Graphs.MinimumNumberOfVertices;
 import Graphs.NumberOfProvinces;
 
 public class Tester {
 
     public static void main(String[] args) {
-        int[][] graph = new int[4][];
-        graph[0] = new int[]{1,0,0,1};
-        graph[1] = new int[]{0,1,1,0};
-        graph[2] = new int[]{0,1,1,1};
-        graph[3] = new int[]{1,0,1,1};
+        // int[][] graph = new int[4][];
+        // graph[0] = new int[]{1,0,0,1};
+        // graph[1] = new int[]{0,1,1,0};
+        // graph[2] = new int[]{0,1,1,1};
+        // graph[3] = new int[]{1,0,1,1};
 
-        NumberOfProvinces num = new NumberOfProvinces();
+        // NumberOfProvinces num = new NumberOfProvinces();
 
-        System.out.println(num.findCircleNum(graph));
+        // System.out.println(num.findCircleNum(graph));
 
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        temp.add(0);
+        temp.add(1);
+        list.add(temp);
+        temp = new ArrayList<>(); 
+        temp.add(0);
+        temp.add(2);
+        list.add(temp);
+        temp = new ArrayList<>(); 
+        temp.add(2);
+        temp.add(5);
+        list.add(temp);
+        temp = new ArrayList<>(); 
+        temp.add(3);
+        temp.add(4);
+        list.add(temp);
+        temp = new ArrayList<>(); 
+        temp.add(4);
+        temp.add(2);
+        list.add(temp);
+        temp = new ArrayList<>(); 
+
+        MinimumNumberOfVertices min = new MinimumNumberOfVertices();
+        temp = min.findSmallestSetOfVertices(6, list);
+        
     }    
 
 }
