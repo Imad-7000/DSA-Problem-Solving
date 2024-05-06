4638af2786d95164b47ce454929f596432af28b5
Imad-7000 <imad.99211@gmail.com>
2024-05-06 12:03:01 +0930

    Valid Anagram

---
+++ src/Strings/ValidAnagram.java
@@ -0,0 +1,47 @@
+/*
+ * 
+ * 
+ * 
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ 
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+ */
+package Strings;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+
+        s = String.valueOf(c1);
+        t = String.valueOf(c2);
+
+        if(s.equals(t))
+            return true;
+        return false;
+    }
+}
