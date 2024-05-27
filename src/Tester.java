d9be2cc625db0c947496f7c423b32e679cf66d7c
Imad-7000 <imad.99211@gmail.com>
2024-05-27 16:47:57 +0930

    someDP

--- src/Tester.java
+++ src/Tester.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 import HeyCoach.SessionTest.LevelTest.UniqueSubsequence;
+import Recursion.DP.GetMax;
 
 public class Tester {
 
     public static void main(String[] args) {
       
-        String s = "abc";
-        UniqueSubsequence u = new UniqueSubsequence();
-        System.out.println(u.distinctSubseqII(s));
+        GetMax g = new GetMax();
+        System.out.println(g.getMaximumGenerated(4));
 
         List<List<Integer>> list = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
