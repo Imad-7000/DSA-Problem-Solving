a07ed915981f46c9c15f1362ad85802fb7225fea
Imad-7000 <imad.99211@gmail.com>
2024-06-10 15:09:34 +0930

    Stone Game 2

--- src/Recursion/DP/StoneGameII.java
+++ src/Recursion/DP/StoneGameII.java
@@ -34,25 +34,52 @@ Constraints:
 1 <= piles[i] <= 104
  * 
  * 
- * 
- * TODO
  */
 package Recursion.DP;
 
 public class StoneGameII {
+    Integer[][] cache;
+    
     public int stoneGameII(int[] piles) {
-        return stoneGameII(piles, 2, 1, 0);
+        
+        cache = new Integer[piles.length][piles.length * 2];
+        
+        int totalStones = 0;
+        for(int stones : piles) totalStones += stones;
+        
+        //(Alice's Stone + Bob's Stone) = totalStones
+        //(Alice's Stone - Bob's Stone) = Alice's score
+        
+        // 2 * Alice's Stone = totalStones + Alice's score
+        //Alice's Stone      = (totalStones + Alice's score) / 2
+        
+        return (totalStones + getAliceScore(piles, 0, 1)) / 2;
     }
+    
+    private int getAliceScore(int[] piles, int index, int M){
+        if(index == piles.length) return 0;
+        
+        
+        if(cache[index][M] != null) return cache[index][M];
+        
+        int maxScore = Integer.MIN_VALUE;
+        
+        int stone = 0;
+        
+        for(int x = 0; x < 2 * M; x++){
+            int i = index + x;
+            
+            if(i >= piles.length) continue;
+            
+            stone += piles[i];
 
-    public int stoneGameII(int[] piles, int X, int M, int idx){
-        if(idx >= piles.length)
-            return 0;
-        int anssss = 0;
-        int max = -1;
-        for(int i = idx; i < 2*M; i++){
-            int ans = piles[i] + stoneGameII(piles, i + 1, Math.max(i + 1,M), i + 1);
-            max = Math.max(ans, max);
+            //calculate score
+            int score = stone - getAliceScore(piles, i + 1, Math.max(x + 1, M));
+            
+            //update score
+            maxScore = Math.max(maxScore, score);
         }
-        return m
+        
+        return cache[index][M] = maxScore;
     }
 }
