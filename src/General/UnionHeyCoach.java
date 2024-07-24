8e36b485bc1e4ee1f72e41419cfd030d01c7c69f
Imad-7000 <imad.99211@gmail.com>
2024-07-24 18:17:59 +0930

    Union Heycoach

---
+++ src/General/UnionHeyCoach.java
@@ -0,0 +1,32 @@
+/*
+ * 
+ * 
+ * 
+ * Union in heycoach
+Two heyCoach students have been assigned to solve certain coding problems for their upcoming tasks , 
+the problems is represented with tme. he help of the problem’s number associated with it,
+ both of them have a list of problems represented by an array of problem’s number(say arr1 and arr2),
+  now as they have very limited time, they want to help each other by solving together. Return how many problems they have to solve together in order to save time.
+
+Example 1
+Input
+ arr1= [1, 2, 3 , 5]
+ arr2 =[7,6,5,8,9] 
+Output
+8
+Example 2
+Input
+ arr1=[2,3]
+ arr2 =[2,3]
+Output
+2
+Constraints:
+1<=arr1.length,arr2.length<=1000 For all 0<= i <arr1.length,arr2.length
+
+1<=arr1[i],arr2[i]<=1000
+ */
+package General;
+
+public class UnionHeyCoach {
+
+}
